9f48bec8106b45129f9a20df620f3ba445445f04
isongeee <nilocalderon0606@gmail.com>
2026-05-01 01:01:30 +0800

    Initial Commit

---
+++ StaxTests/StaxTests.swift
@@ -0,0 +1,19 @@
+//
+//  StaxTests.swift
+//  StaxTests
+//
+//  Created by Nilo Calderon on 5/1/26.
+//
+
+import Testing
+@testable import Stax
+
+struct StaxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
